0ee9d8c900e8188bc01b4a55af9a2c2ea420ce10
KamranYusupov <kamrankek48@gmail.com>
2026-05-18 15:40:37 +0400

    рефакторирнг settings

--- app/app/core/config.py
+++ app/app/core/config.py
@@ -24,8 +24,8 @@ class Settings(BaseSettings):
 
     # region Настройки бота
     bot_token: str = Field(title="Токен бота")
-    bot_name: str | None = Field(title="Имя бота", default=None)
-    bot_link: str = Field(title="Ссылка на бота")
+    bot_name: str = Field(title="Имя бота(username)")
+    bot_link: str = Field(title="Ссылка на бота", default="https://t.me/{bot_name}")
     chat_id: int = Field(title="ID чата")
     chat_link: str = Field(title="Ссылка на чат")
     channel_id: str = Field(title="ID канала")
@@ -42,13 +42,17 @@ class Settings(BaseSettings):
     timezone: str = Field(default="Europe/Moscow")
     send_donate_for_registration: bool = Field(default=False)
     donate_for_registration: int = Field(default=1)
+    withdrawal_min_tokens_count: int = Field(
+        title="Минимальное количество токенов для вывода",
+        default=10
+    )
     # endregion
 
     # region API
     api_prefix: str = Field(title="Префикс API", default="/api")
     # endregion
 
-    debug: bool = Field(title="Режим отладки", default=True)
+    debug: bool = Field(title="Режим отладки")
     secret_key: str = Field(
         title="Секретный ключ", default_factory=lambda: secrets.token_hex(16)
     )
@@ -62,16 +66,6 @@ class Settings(BaseSettings):
     database_url: PostgresDsn | None = Field(title="Ссылка БД", default=None)
     # endregion
 
-    # region Настройки RabbitMQ
-    rabbitmq_host: str = Field(title="Хост rabbitmq", default="guest:guest@rabbitmq")
-    rabbitmq_port: int | str = Field(title="Порт rabbitmq", default=5672)
-    # endregion
-
-    # region Настройки Redis
-    redis_host: str = Field(title="Хост redis", default="redis")
-    redis_port: int | str = Field(title="Порт redis", default=6379)
-    # endregion
-
     # region Настройки CryptoBot
     crypto_bot_api_token: str = Field(title="CryptoBot API token")
     crypto_bot_api_base_url: str = Field(
@@ -80,23 +74,21 @@ class Settings(BaseSettings):
     )
     # endregion
 
-    add_bot_to_matrix_1_countdown_minutes: int = 5
-    add_bot_to_matrix_2_countdown_minutes: int = 15
+    # region Настройки проценты донатов
+    first_sponsor_donate_percent: int = Field(title="Процент доната первому спонсору", default=20)
+    second_sponsor_donate_percent: int = Field(title="Процент доната второму спонсору", default=10)
+    third_sponsor_donate_percent: int = Field(title="Процент доната третьему спонсору", default=5)
+    matrix_donate_percent: int = Field(title="Процент доната для матрицы", default=10)
+    # endregion
 
-    first_sponsor_donate_percent: int = 20
-    second_sponsor_donate_percent: int = 10
-    third_sponsor_donate_percent: int = 5
-
-    matrix_donate_percent: int = 10
-
-    withdrawal_min_tokens_count: int = 10
-
-    level_length: int = 2
-    second_level_length: int = 4
-    third_level_length: int = 8
-    fourth_level_length: int = 16
-    matrix_max_length: int = 30
-    matrix_max_level: int = 4
+    # region Настройки длины бинарной матрицы в 4ой степени (2⁴)
+    level_length: int = Field(title="Длина первого уровня матрицы", default=2)
+    second_level_length: int = Field(title="Длина второго уровня матрицы", default=4)
+    third_level_length: int = Field(title="Длина третьего уровня матрицы", default=8)
+    fourth_level_length: int = Field(title="Длина четвертого уровня матрицы", default=16)
+    matrix_max_length: int = Field(title="Максимальная длина матрицы", default=30)
+    matrix_max_level: int = Field(title="Максимальный уровень матрицы", default=4)
+    # endregion
 
     # region Настройки Telegram server
     telegram_server_host: str = Field(
@@ -118,6 +110,16 @@ class Settings(BaseSettings):
     math_captcha_options_count: int = Field(default=6)
     math_captcha_max_attempts_count: int = Field(default=2)
     # endregion
+
+
+    add_bot_to_matrix_1_countdown_minutes: int = Field(
+        title="Время ожидания первой задачи добавления бота в матрицу",
+        default=5
+    )
+    add_bot_to_matrix_2_countdown_minutes: int = Field(
+        title="Время ожидания второй задачи добавления бота в матрицу",
+        default=15
+    )
 
     @computed_field
     @property
@@ -142,27 +144,6 @@ class Settings(BaseSettings):
             port=self.postgres_port,
             path=f"{self.postgres_db}",
         )
-
-    @computed_field
-    @property
-    def rabbitmq_url(self) -> str:
-        return f"amqp://{self.rabbitmq_host}:{self.rabbitmq_port}/"
-
-    @computed_field
-    @property
-    def redis_url(self) -> str:
-        return f"redis://{self.redis_host}:{self.redis_port}/"
-
-    @computed_field
-    @property
-    def celery_broker_url(self) -> str:
-        return self.rabbitmq_url
-
-    @computed_field
-    @property
-    def celery_backend_url(self) -> str:
-        return f"{self.redis_url}/0"
-
 
 
 class Config:
