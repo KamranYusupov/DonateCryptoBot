889c9a94a6e8dcc8ea4c2d575bdb3aca2807ad2a
KamranYusupov <kamrankek48@gmail.com>
2026-05-18 14:56:36 +0400

    добавил схемы AdminStatisticSchema и UpdateAdminStatisticSchema

---
+++ app/app/schemas/statistic.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+digit_type = float | int
+
+
+class AdminStatisticSchema(BaseModel):
+    system_bill: digit_type
+    donates_sum_for_registration: digit_type
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UpdateAdminStatisticSchema(BaseModel):
+    system_bill: Optional[digit_type] = None
+    donates_sum_for_registration: Optional[digit_type] = None
+
