12ef5ad57e5edb9aef6e94df1b80fd4ddaecafbf
KamranYusupov <kamrankek48@gmail.com>
2026-06-06 23:09:18 +0400

    create_donates field in tasks

--- app/app/middlewares/ban_user.py
+++ app/app/middlewares/ban_user.py
@@ -19,9 +19,51 @@ async def ban_user_middleware(
     """
     Middleware, для обработки действий от забаненного пользователя.
     """
+    import loguru
+
     current_user = await telegram_user_service.get_telegram_user(
         user_id=event.from_user.id
     )
+    if not isinstance(event, CallbackQuery):
+        pass
+    else:
+        loguru.logger.info(event.data)
+        if (event.data.startswith("ref_msg_")
+                or event.data == "skip_referrals_msg_state"
+                or event.data == "send_complete_message"
+                or event.data in ("confirm_referrals_send", "create_message")
+        ):
+            # Если нажал НЕ админ — выходим
+            if not current_user or not current_user.is_admin:
+                return
+
+        # Если кнопка НЕ админская, она идет на общую проверку для всех
+        elif not (
+                (event.data in ("confirm_donate_🟢_500", "donate_🟢_500", "donations", "excel_users", "cancel",
+                                "confirm_transfer", "start_transfer"))
+                or
+                (event.data.startswith("transfer"))
+                or
+                (event.data.startswith("send_donate_🟢_500_"))
+                or
+                (event.data.startswith("donate_🟢_500_"))
+                or
+                (event.data.startswith("transfer"))
+                or
+                (event.data.startswith("referrals_"))
+                or
+                (event.data.startswith("register_"))
+                or
+                (event.data.startswith("yes_"))
+                or
+                (event.data.startswith("menu_"))
+                or
+                (event.data.startswith("team_"))
+                or
+                (event.data.startswith("archive_team_"))
+        ):
+            return
+
     if not current_user:
         return await handler(event, data)
     if current_user.is_banned:
