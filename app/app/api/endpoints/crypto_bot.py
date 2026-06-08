f504d5e0171aa48782d1ccf9b1aadea4ad1f31d4
KamranYusupov <kamrankek48@gmail.com>
2026-06-08 14:43:20 +0400

    Добавил защиту от дублей

--- app/app/api/endpoints/crypto_bot.py
+++ app/app/api/endpoints/crypto_bot.py
@@ -29,9 +29,9 @@ async def updates_webhook(
         telegram_user_service: TelegramUserService = Depends(
             Provide[Container.telegram_user_service]
         ),
-        crypto_bot_api_service: CryptoBotAPIService = Provide[
-            Container.crypto_bot_api_service
-        ],
+        crypto_bot_api_service: CryptoBotAPIService = Depends(
+            Provide[Container.crypto_bot_api_service],
+        )
 ) -> Response:
     if body.update_type != "invoice_paid":
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
