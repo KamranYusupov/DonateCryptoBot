6d6857a058b9939002479af3eb77d2c69f7a6d9b
KamranYusupov <kamrankek48@gmail.com>
2026-06-08 15:49:51 +0400

    crypto_bot_processed_webhook_service

---
+++ app/app/services/crypto_bot_processed_webhook_service.py
@@ -0,0 +1,19 @@
+from app.api.schemas.crypto_bot import UpdateWebhookSchema
+from app.repositories.processed_crypto_bot_payment_webhook import RepositoryProcessedCryptoBotPaymentWebhook
+
+
+class CryptoBotProcessedWebhookService:
+    def __init__(
+            self,
+            repository_processed_webhook: RepositoryProcessedCryptoBotPaymentWebhook,
+    ) -> None:
+        self._repository_processed_webhook = repository_processed_webhook
+
+    async def get_by_update_id(self, update_id: int):
+        return self._repository_processed_webhook.get(update_id=update_id)
+
+    async def exists(self, update_id: int):
+        return self._repository_processed_webhook.exists(update_id=update_id)
+
+    async def create_by_request_body(self, body: UpdateWebhookSchema):
+        return self._repository_processed_webhook.create(obj_in=body.model_dump())
