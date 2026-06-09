a1ef5e85f87401a1632945e7a8a25d7aab886c61
KamranYusupov <kamrankek48@gmail.com>
2026-06-09 17:11:57 +0400

    imports fixes

---
+++ app/app/services/__init__.py
@@ -0,0 +1,30 @@
+from app.services.donate_confirm_service import DonateConfirmService
+from app.services.registration_contest_service import RegistrationContestService
+from app.services.telegram_user_service import TelegramUserService
+from app.services.legacy_matrix_service import MatrixService
+from app.services.donate_service import DonateService
+from app.services.crypto_bot_api_service import CryptoBotAPIService
+from app.services.crypto_bot_processed_webhook_service import CryptoBotProcessedWebhookService
+from app.services.telegram_user_service import TelegramUserService
+from app.services.withdrawal_request import WithdrawalRequestService
+from app.services.add_bot_to_matrix_task_service import AddBotToMatrixTaskService
+from app.services.sponsors_contest_service import SponsorsContestService
+from app.services.transfer_service import TransferService
+from app.services.statistic_service import AdminStatisticService
+from app.services.matrix_node_service import MatrixNodeService
+
+__all__ = (
+    "TelegramUserService",
+    "CryptoBotProcessedWebhookService",
+    "DonateService",
+    "DonateConfirmService",
+    "RegistrationContestService",
+    "SponsorsContestService",
+    "CryptoBotAPIService",
+    "WithdrawalRequestService",
+    "AddBotToMatrixTaskService",
+    "TransferService",
+    "AdminStatisticService",
+    "MatrixService",
+    "MatrixNodeService",
+)
