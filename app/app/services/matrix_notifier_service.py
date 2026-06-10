4b1c1bf8ef0dabf5349d25a3007ad84f215e3551
KamranYusupov <kamrankek48@gmail.com>
2026-06-10 16:53:11 +0400

    fixes

--- app/app/services/matrix_notifier_service.py
+++ app/app/services/matrix_notifier_service.py
@@ -1,8 +1,7 @@
-import uuid
-
+from app.keyboards.donate import get_donate_keyboard
 from app.models.telegram_user import DonateStatus
 from app.repositories.telegram_user import RepositoryTelegramUser
-from app.tasks.taskiq.telegram import send_message_task
+from app.tasks.taskiq.infra.telegram import send_message_task
 from app.utils.texts import get_sponsor_activation_text
 
 
@@ -31,10 +30,15 @@ class MatrixNotifierService:
         invited_users = self._repository_telegram_user.list(
             sponsor_user_id=sponsor_user_id,
         )
+
+        reply_markup = get_donate_keyboard(
+            buttons={"⚡️ Активировать площадку": "donations"}
+        )
         for user in invited_users:
             await send_message_task.kiq(
                 chat_id=user.user_id,
-                text=notification_text
+                text=notification_text,
+                reply_markup=reply_markup,
             )
 
 
