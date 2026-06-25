988df77a3f0678e610e6e4aee80d7df54be1943c
KamranYusupov <kamrankek48@gmail.com>
2026-06-25 17:31:55 +0400

    RepositoryTriumphBillTransaction order methods added

--- app/app/repositories/triumph_bill_transaction.py
+++ app/app/repositories/triumph_bill_transaction.py
@@ -1,8 +1,48 @@
+from typing import Sequence, Optional
+import uuid
+
+from sqlalchemy import desc
+from sqlalchemy.sql import select
+
 from app.models import TriumphBillTransaction
 from app.repositories.base import RepositoryBase
+from app.repositories.base.mixins import CountMixin
 
 
 class RepositoryTriumphBillTransaction(
-    RepositoryBase[TriumphBillTransaction]
+    RepositoryBase[TriumphBillTransaction],
+    CountMixin,
 ):
-    pass
+    def get_ordered_ids(
+            self,
+            limit: Optional[int] = None,
+            offset: Optional[int] = None,
+            **kwargs
+    ) -> Sequence[uuid.UUID]:
+        statement = (
+            select(TriumphBillTransaction.id)
+            .filter_by(**kwargs)
+            .order_by(desc(TriumphBillTransaction.created_at))
+            .limit(limit)
+            .offset(offset)
+        )
+
+        result = self._session.execute(statement)
+        return result.scalars().all()
+
+    def get_ordered_transactions(
+            self,
+            limit: Optional[int] = None,
+            offset: Optional[int] = None,
+            **kwargs
+    ) -> Sequence[TriumphBillTransaction]:
+        statement = (
+            select(TriumphBillTransaction)
+            .filter_by(**kwargs)
+            .order_by(desc(TriumphBillTransaction.created_at))
+            .limit(limit)
+            .offset(offset)
+        )
+
+        result = self._session.execute(statement)
+        return result.scalars().all()
