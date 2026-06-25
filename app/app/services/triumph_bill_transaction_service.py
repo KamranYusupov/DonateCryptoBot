eee8e05459295efb04a54c56604c0c5012dcd53e
KamranYusupov <kamrankek48@gmail.com>
2026-06-25 17:32:47 +0400

    order methods added to TriumphBillTransactionService

--- app/app/services/triumph_bill_transaction_service.py
+++ app/app/services/triumph_bill_transaction_service.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import uuid
+from typing import Optional, Sequence
 from uuid import UUID
 
 from app.models import TriumphBillTransaction, TriumphBillTransactionType
@@ -13,3 +14,36 @@ class TriumphBillTransactionService(CrudServiceMixin[RepositoryTriumphBillTransa
     ):
         super().__init__(repository=repository_triumph_bill_transaction)
         self._repository_triumph_bill_transaction = repository_triumph_bill_transaction
+
+    async def get_ordered_ids(
+            self,
+            limit: Optional[int] = None,
+            offset: Optional[int] = None,
+            **kwargs,
+    ) -> Sequence[uuid.UUID]:
+        return (
+            self._repository_triumph_bill_transaction
+            .get_ordered_ids(
+                limit=limit,
+                offset=offset,
+                **kwargs
+            )
+        )
+
+    async def get_ordered_transactions(
+            self,
+            limit: Optional[int] = None,
+            offset: Optional[int] = None,
+            **kwargs
+    ) -> Sequence[TriumphBillTransaction]:
+        return (
+            self._repository_triumph_bill_transaction
+            .get_ordered_transactions(
+                limit=limit,
+                offset=offset,
+                **kwargs
+            )
+        )
+
+    async def get_count(self, **kwargs):
+        return self._repository_triumph_bill_transaction.get_count(**kwargs)
