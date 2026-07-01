a1f96b5376280e1b799267a4113b5de2e89213f4
KamranYusupov <kamrankek48@gmail.com>
2026-07-01 11:06:09 +0400

    add count field to SponsorsContestPoint

--- app/app/models/contest.py
+++ app/app/models/contest.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 from sqlalchemy import (
     Column,
     Numeric,
+    Integer,
 )
 from sqlalchemy.sql import func, text
 
@@ -23,13 +24,23 @@ class SponsorsContestPoint(
 ):
     __tablename__ = "sponsors_contest_points"
 
+    count = Column(Integer, index=True, default=1, server_default=text("1"))
+
 
 
 class RegistrationContest(Base, AbstractContest, UUIDMixin):
     __tablename__ = "registration_contests"
 
-    prize_fund = Column(Numeric(18, 6, asdecimal=True), default=Decimal("0.0"), server_default=text("0"))
-    init_prize_fund = Column(Numeric(18, 6, asdecimal=True), default=Decimal("0.0"), server_default=text("0"))
+    prize_fund = Column(
+        Numeric(18, 6, asdecimal=True),
+        default=Decimal("0.0"),
+        server_default=text("0"),
+    )
+    init_prize_fund = Column(
+        Numeric(18, 6, asdecimal=True),
+        default=Decimal("0.0"),
+        server_default=text("0"),
+    )
 
 
 class RegistrationContestPoint(Base,
