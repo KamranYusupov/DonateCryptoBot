9411ed9143d796f037fb8e59587a7f05f78dea71
KamranYusupov <kamrankek48@gmail.com>
2026-09-01 23:13:25 +0400

    crypto increment bill webhook fix

--- app/orchestrators/crypto_bot_payment.py
+++ app/orchestrators/crypto_bot_payment.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from app.api.schemas.crypto_bot import CryptoInvoiceSchema, UpdateWebhookSchema, InvoicePayloadSchema
-from app.models.telegram_user import BillType
+from app.models.telegram_user import BillType, TelegramUser
 from app.services import (
     TelegramUserService,
     CryptoBotProcessedWebhookService,
@@ -14,6 +14,7 @@ from app.tasks.taskiq.tasks.infra.telegram import (
 from app.tasks.taskiq.tasks.business.donations import (
     send_donations_menu_task
 )
+from app.models.matrix import MatrixMarketingType
 
 
 class CryptoBotPaymentOrchestrator:
@@ -30,13 +31,13 @@ class CryptoBotPaymentOrchestrator:
             webhook_body: UpdateWebhookSchema,
             invoice_request: CryptoInvoiceSchema,
     ) -> None:
-        await self._increment_bill_and_save_webhook(
+        telegram_user = await self._increment_bill_and_save_webhook(
             webhook_body,
             invoice_request,
         )
         payload = invoice_request.payload
         await self._apply_bot_actions_after_successful_payment(
-            chat_id=payload.telegram_id,
+            telegram_user=telegram_user,
             tokens_count=payload.tokens_count,
             messages_to_delete_ids=payload.messages_to_delete_ids,
         )
@@ -45,7 +46,7 @@ class CryptoBotPaymentOrchestrator:
             self,
             webhook_body: UpdateWebhookSchema,
             invoice_request: CryptoInvoiceSchema,
-    ) -> None:
+    ) -> TelegramUser:
         telegram_id = invoice_request.payload.telegram_id
         tokens_count = invoice_request.payload.tokens_count
 
@@ -57,29 +58,32 @@ class CryptoBotPaymentOrchestrator:
         )
 
         await self._processed_webhook_service.create_by_request_body(body=webhook_body)
+        return telegram_user
 
     @staticmethod
     async def _apply_bot_actions_after_successful_payment(
-            chat_id: int,
+            telegram_user: TelegramUser,
             tokens_count: int,
             messages_to_delete_ids: list[int],
     ) -> None:
         for message_id in messages_to_delete_ids:
             await delete_message_task.kiq(
-                chat_id=chat_id,
+                chat_id=telegram_user.user_id,
                 message_id=message_id,
             )
 
         await send_message_task.kiq(
-            chat_id=chat_id,
+            chat_id=telegram_user.user_id,
             text="Оплата прошла успешно ✅\n\n"
                  f"На баланс зачислено {tokens_count} USDT.",
             reply_markup=get_reply_keyboard(None) # FIXME
         )
 
         await send_donations_menu_task.kiq(
-            chat_id=chat_id,
-            current_user_id=chat_id,
+            chat_id=telegram_user.user_id,
+            current_user_id=telegram_user.id,
+            marketing_type_name=MatrixMarketingType.GLOBAL.name,
+            status_name=telegram_user.global_marketing_status.name,
         )
 
 
